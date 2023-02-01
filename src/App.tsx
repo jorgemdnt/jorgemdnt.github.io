fad527e529ae10714974e6852ec5b3807744998e
Jorge Modesto <7728049+jorgemdnt@users.noreply.github.com>
2023-02-01 23:01:36 +0100

    Update homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <div className='container'>
+        <h1>Hi, I'm Jorge 👋</h1>
+        <hr/>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          I'm an experienced full-stack engineer with a focus on backend and strong leadership skills.
+          Adept in multiple programming languages and frameworks.
+          Proven track record of delivering high-quality solutions and improving processes.
+          Committed to ongoing education and mentorship.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          Today I work mainly with Ruby, Rails, Typescript and React on <a className='App-link' href='https://www.catawiki.com/en/'>Catawiki</a>.
+        </p>
+        <h2>Links</h2>
+        <ul>
+          <li>linkedin <a className='App-link' href='https://www.linkedin.com/in/jorge-modesto-neto/'>/in/jorge-modesto-neto</a></li>
+          <li>github <a className='App-link' href='https://github.com/jorgemdnt'>@jorgemdnt</a></li>
+        </ul>
+      </div>
     </div>
   );
 }
